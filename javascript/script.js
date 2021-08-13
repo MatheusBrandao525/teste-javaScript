2d51965caf173937153189233db5d13e05adb486
MatheusBrandao525 <mafe123silva@gmail>
2021-08-12 22:15:22 -0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+var numero1=null;
+
+function funcao1(){
+    //alert("assim que usa um alert!")
+    //console.log("hello word")
+    numero1+=1;{
+        //console.log(numero1)
+        alert(numero1);
+    }
+}
