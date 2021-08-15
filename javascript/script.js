8de08a83a83b05e3fe004a22c75caae59815c9e3
MatheusBrandao525 <mafe123silva@gmail>
2021-08-15 09:56:39 -0300

    atualização

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,12 @@
 var numero1=null;
 
+var resultado = document.getElementById('tela')
 function funcao1(){
     //alert("assim que usa um alert!")
     //console.log("hello word")
     numero1+=1;{
         //console.log(numero1)
-        alert(numero1);
+        resultado.textContent= `${numero1}`;
     }
 }
+
